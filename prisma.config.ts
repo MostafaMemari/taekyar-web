616220a64ce2ebcb805f5c2f5f249940480d4a51
MostafaMemari <m.memari711@gmail.com>
2026-08-31 21:37:13 +0330

    feat: update Prisma seed script path and remove unused config comments

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,26 @@
+// import { defineConfig, env } from "prisma/config";
+
+// export default defineConfig({
+//   schema: "prisma/schema.prisma",
+//   migrations: {
+//     path: "prisma/migrations",
+//     seed: "node --env-file=.env prisma/seed.ts",
+//   },
+//   datasource: {
+//     url: env("DATABASE_URL"),
+//   },
+// });
+
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
+
   migrations: {
     path: "prisma/migrations",
-    seed: "node --env-file=.env prisma/seed.ts",
+    seed: "node prisma/seed.ts",
   },
+
   datasource: {
     url: env("DATABASE_URL"),
   },
